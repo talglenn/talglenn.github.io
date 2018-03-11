b7d583993777a75cd4ab6c0d7d99fb2f0939f96b
Talmadge Glenn <glenn.t88@gmail.com>
2018-03-10 21:43:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction1() 
+{
+    var x = document.getElementById("myNavMenu");
+    if (x.className === "nav-menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-menu";
+    }
+}
+
+// $("#first").flip();
+// $("#second").flip();
+// $("#third").flip();
+// $("#fourth").flip();
+
+//Sticky nav code
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("myNavMenu");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
