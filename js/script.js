b3c9bbe70c820826132be811e2f06bd3d319c116
Talmadge Glenn <glenn.t88@gmail.com>
2018-03-13 20:23:43 -0400

    Replaced live clock image with the actual clock.

--- js/script.js
+++ js/script.js
@@ -57,3 +57,34 @@ $(document).ready(function(){
 
   var piechart = new Chart(ctx).Pie(data);
 });
+
+//Clock
+
+function clock(){
+  const fullDate = new Date();
+  var hours = fullDate.getHours();
+  if (hours > 12) {
+        hours -= 12;
+    } else if (hours === 0) {
+        hours = 12;
+    }
+
+  var mins = fullDate.getMinutes();
+  var secs = fullDate.getSeconds();
+
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+  if(mins < 10){
+    mins = "0" + mins;
+  }
+  if(secs < 10){
+    secs = "0" + secs;
+  }
+
+  document.getElementById('hour').innerHTML =  hours;
+  document.getElementById('minute').innerHTML = ":" + mins;
+  document.getElementById('second').innerHTML = ":" + secs;
+}
+
+setInterval(clock, 100);
